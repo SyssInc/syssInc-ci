cf113fb7956a4e4306807c80618e3eb31504cf0d
Alysson Sousa <alyssonvitor500@gmail.com>
2025-09-12 10:59:40 -0300

    feat: Ajustar a soma padrão

--- src/main/java/com/java/ci/Main.java
+++ src/main/java/com/java/ci/Main.java
@@ -3,6 +3,6 @@ package com.java.ci;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(MathUtils.sum(10, 20));
+        System.out.println(MathUtils.sum(130, 20));
     }
 }
